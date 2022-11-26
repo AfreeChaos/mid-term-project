454c6f85f11aadccbd458594841282b7c824bf5c
Àfrica Calderón Alcaraz <africa@imac-de-africa.home>
2022-11-26 20:25:17 +0100

    26/11.3

--- js/project.js
+++ js/project.js
@@ -1,42 +1,66 @@
 
+/* Cards in Projects page */
 
-async function card1Data2() {
+async function getDataProjects() {
     const res = await fetch('https://api.punkapi.com/v2/beers');
     const finalRes = await res.json();
     console.log(finalRes)
 
-    /*Card 1 - Home*/
+    /*Main - Projects*/
 
-    const card1Title = document.getElementById("card4-title")
+    const projectTitle = document.getElementById("project-title")
+    projectTitle.innerText = finalRes[0].name
+
+    const projectTagline = document.getElementById("project-tagline")
+    projectTagline.innerText = finalRes[0].tagline
+
+    const projectTips = document.getElementById("project-tips")
+    projectTips.innerText = finalRes[0].brewers_tips
+
+    const projectDescription = document.getElementById("project-description")
+    projectDescription.innerText = finalRes[0].description
+
+
+    /*Card 1 - Projects*/
+
+    const card1Title = document.getElementById("card1-title")
     card1Title.innerText = finalRes[0].name
 
-    const card1Tagline = document.getElementById("card4-tagline")
+    const card1Tagline = document.getElementById("card1-tagline")
     card1Tagline.innerText = finalRes[0].tagline
 
-    const card1Image= document.getElementById("card4-img")
+    const card1Image= document.getElementById("card1-img")
     card1Image.src = finalRes[0].image_url
 
-    /*Card 2 - Home*/
+    /*Card 2 - Projects*/
 
-    const card2Title = document.getElementById("card5-title")
+    const card2Title = document.getElementById("card2-title")
     card2Title.innerText = finalRes[1].name
     
-    const card2Tagline = document.getElementById("card5-tagline")
+    const card2Tagline = document.getElementById("card2-tagline")
     card2Tagline.innerText = finalRes[1].tagline
 
-    const card2Image= document.getElementById("card5-img")
+    const card2Image= document.getElementById("card2-img")
     card2Image.src = finalRes[0].image_url
 
-    /*Card 3 - Home*/
+    /*Card 3 - Projects*/
 
-    const card3Title = document.getElementById("card6-title")
+    const card3Title = document.getElementById("card3-title")
     card3Title.innerText = finalRes[3].name
 
-    const card3Tagline = document.getElementById("card6-tagline")
+    const card3Tagline = document.getElementById("card3-tagline")
     card3Tagline.innerText = finalRes[3].tagline
 
-    const card3Image= document.getElementById("card6-img")
+    const card3Image= document.getElementById("card3-img")
     card3Image.src = finalRes[0].image_url
 }
 
-card1Data2()
+getDataProjects()
+
+
+/* Date in Projects Page */
+
+function dateToday() {
+}
+
+dateToday()
